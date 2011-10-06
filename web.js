31c17a87ce6b4458cb6a02b3d6d58ad558fdf756
Canna Wen <cannawen@gmail.com>
2011-10-05 20:38:25 -0400

    Added /canna

--- web.js
+++ web.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/canna', function(request, response) {
+  response.send('Hello Canna!');
+});
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
