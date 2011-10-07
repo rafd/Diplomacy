269e4891910afd099318d0492fdffa2857e64b1e
Rafal Dittwald <rafal.dittwald@gmail.com>
2011-10-07 17:27:14 -0400

    log with backbone + local storage

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,110 @@
-require(["jquery", "jquery.alpha", "jquery.beta"], function($) {
-    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
-    $(function() {
-        $('body').alpha().beta();
+require(["jquery", "scripts/underscore.min.js", "scripts/backbone.min.js", "scripts/backbone-localstorage.js","scripts/ICanHaz.min.js"], function($) {
+  $(function() {
+    
+    // MODELS
+
+    window.LogItem = Backbone.Model.extend({
+      defaults: function(){
+        return {
+          content: 'default content'
+        }
+      },
+      initialize: function (spec) {
+        if (!spec || !spec.content) {
+          throw "InvalidConstructArgs";
+        }
+        this.set({
+            htmlId: 'logitem_' + this.cid
+        });
+      },
+      validate: function (attrs) {
+        if (attrs.content) {
+          if (!_.isString(attrs.content) || attrs.content.length === 0 ) {
+            return "Title must be a string with a length";
+          }
+        }
+      }
     });
+
+
+    // COLLECTIONS
+
+    window.LogItems = Backbone.Collection.extend({
+      model: LogItem,
+      localStorage: new Store("logitems"),
+      /*all: function(){
+        return this;
+      },*/
+      initialize: function() {
+        //
+      }
+    });
+
+    window.Log = new LogItems();
+
+    // VIEWS
+
+    window.LogView = Backbone.View.extend({
+      tagName: "div",
+      template: _.template("<%= content %>"),
+      events: {
+        "click" : "alert"
+      },
+      initialize: function() {
+        // bindings
+      },
+      render: function() {
+        $(this.el).html(this.template(this.model.toJSON()));
+        return this;
+      },
+      alert: function() {
+        alert('i been clicked!');
+      }
+    });
+
+
+
+    // APP
+
+    window.AppView = Backbone.View.extend({
+      el: $("#diplomacyapp"),
+      events: {
+        "keypress #new-logitem":  "createOnEnter",
+        "keyup #new-logitem": "console"
+      },
+      initialize: function() {
+        this.input = this.$("#new-logitem");
+
+        Log.bind('add', this.addOne, this);
+        Log.bind('reset', this.addAll, this);
+        Log.bind('all', this.render, this);
+
+        Log.fetch();
+      },
+      render: function() {
+      },
+      addOne: function(logitem) {
+        var view = new LogView({model: logitem});
+        this.$("#log").append(view.render().el);
+      },
+      addAll: function() {
+        Log.each(this.addOne);
+      },
+      createOnEnter: function(e) {
+        var content = this.input.val();
+        if (!content || e.keyCode != 13) return;
+        Log.create({content: content});
+        this.input.val('');
+      },
+      console: function() {
+        console.log('keyup, bitch');
+      }
+
+    });
+
+
+    window.App = new AppView();
+
+
+  });
 });
